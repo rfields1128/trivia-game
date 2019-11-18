0486bae641c8e1ce9205273bd5f8db306ac411d6
Randall <rfields1128@yahoo.com>
2019-11-17 22:37:25 -0600

    committing to the repository

---
+++ assets/javascript/app.js
@@ -0,0 +1,289 @@
+var number = 55;
+
+var intervalId;
+
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
+
+// array for the questions and answers
+var questions = [{
+    question: "When did World War I begin?",
+    answerList: ["1914", "1917", "1927", "1886"],
+    answer: 0
+},{
+    question: "Who was the 27th president?",
+    answerList: ["Henry Ford", "Johnny Depp", "William Howard Taft", "Barack Obama"],
+    answer: 2
+},{
+    question: "Who invented the lightbulb?",
+    answerList: ["Thomas Jefferson", "Albert Einstein", "Hiram Maxim", "Thomas Edison"],
+    answer: 3
+},{
+    question: "What is the largest continent?",
+    answerList: ["Europe", "Africa", "North America", "Asia"],
+    answer: 3
+},{
+    question: "Which European explorer first discovered America?",
+    answerList: ["Christopher Columbus", "The Vikings", "John Stamos", "Paul Revere"],
+    answer: 1
+},{
+    question: "Why does the Liberty Bell have a crack in it?",
+    answerList: ["Shoddy craftsmanship", "Overenthusiastic patriots", "Lightning strike", "Someone hit it"],
+    answer: 0
+},{
+    question: "Joseph Stalin was the premiere of which country?",
+    answerList: ["Germany", "Ukraine", "Russia", "Soviet Union"],
+    answer: 3
+}];
+// the click button 
+$("#start").on("click", function() {
+
+    // to hide the start button
+    $(this).hide();
+
+    // the countdown for time left on trivia
+    $("#time").html("<h2>Time Remaining: 55 Seconds</h2>" + "<br>");
+
+    // the start timer countdown
+    run();
+   
+    // the will display questions, this will be for question 1 
+    $("#question1").html("<h3>" + questions[0].question + "</h3>");
+    $("#answer1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
+        + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answerList[1] + "</label>"
+        + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answerList[2] + "</label>"
+        + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answerList[3] + "</label><br><br>"
+    );
+
+    // this is for question 2
+    $("#question2").html("<h3>" + questions[1].question + "</h3>");
+    $("#answer2").html("<input type='radio' name='answer2' value='0'>" + "<label>" + questions[1].answerList[0] + "</label>"
+        + "<input type='radio' name='answer2' value='1'>" + "<label>" + questions[1].answerList[1] + "</label>"
+        + "<input type='radio' name='answer2' value='2'>" + "<label>" + questions[1].answerList[2] + "</label>"
+        + "<input type='radio' name='answer2' value='3'>" + "<label>" + questions[1].answerList[3] + "</label><br><br>"
+    );
+
+    // this is for question 3
+    $("#question3").html("<h3>" + questions[2].question + "</h3>");
+    $("#answer3").html("<input type='radio' name='answer3' value='0'>" + "<label>" + questions[2].answerList[0] + "</label>"
+        + "<input type='radio' name='answer3' value='1'>" + "<label>" + questions[2].answerList[1] + "</label>"
+        + "<input type='radio' name='answer3' value='2'>" + "<label>" + questions[2].answerList[2] + "</label>"
+        + "<input type='radio' name='answer3' value='3'>" + "<label>" + questions[2].answerList[3] + "</label><br><br>"
+    );
+
+    // this is for question 4
+    $("#question4").html("<h3>" + questions[3].question + "</h3>");
+    $("#answer4").html("<input type='radio' name='answer4' value='0'>" + "<label>" + questions[3].answerList[0] + "</label>"
+        + "<input type='radio' name='answer4' value='1'>" + "<label>" + questions[3].answerList[1] + "</label>"
+        + "<input type='radio' name='answer4' value='2'>" + "<label>" + questions[3].answerList[2] + "</label>"
+        + "<input type='radio' name='answer4' value='3'>" + "<label>" + questions[3].answerList[3] + "</label><br><br>"
+    );
+
+    // this is for question 5
+    $("#question5").html("<h3>" + questions[4].question + "</h3>");
+    $("#answer5").html("<input type='radio' name='answer5' value='0'>" + "<label>" + questions[4].answerList[0] + "</label>"
+        + "<input type='radio' name='answer5' value='1'>" + "<label>" + questions[4].answerList[1] + "</label>"
+        + "<input type='radio' name='answer5' value='2'>" + "<label>" + questions[4].answerList[2] + "</label>"
+        + "<input type='radio' name='answer5' value='3'>" + "<label>" + questions[4].answerList[3] + "</label><br><br>"
+    );
+    // this is for question 6
+    $("#question6").html("<h3>" + questions[5].question + "</h3>");
+    $("#answer6").html("<input type='radio' name='answer6' value='0'>" + "<label>" + questions[5].answerList[0] + "</label>"
+        + "<input type='radio' name='answer6' value='1'>" + "<label>" + questions[5].answerList[1] + "</label>"
+        + "<input type='radio' name='answer6' value='2'>" + "<label>" + questions[5].answerList[2] + "</label>"
+        + "<input type='radio' name='answer6' value='3'>" + "<label>" + questions[5].answerList[3] + "</label><br><br>"
+    );
+    // this is for question 7
+    $("#question7").html("<h3>" + questions[6].question + "</h3>");
+    $("#answer7").html("<input type='radio' name='answer7' value='0'>" + "<label>" + questions[6].answerList[0] + "</label>"
+        + "<input type='radio' name='answer7' value='1'>" + "<label>" + questions[6].answerList[1] + "</label>"
+        + "<input type='radio' name='answer7' value='2'>" + "<label>" + questions[6].answerList[2] + "</label>"
+        + "<input type='radio' name='answer7' value='3'>" + "<label>" + questions[6].answerList[3] + "</label><br><br>"
+    );
+
+    // this my submit button for when i complete answering the questions and have chosen my answers
+    $("#submit").html("<button id='done' class='btn'>Done</button>");
+
+    // the click event runs keepingScore() and displayResults() when user clicks the done button
+    $("#done").on("click", function() {
+
+        // this will keep track of score based on correct, incorrect, and unanswered
+        keepingScore();
+
+        // this will display the results
+        displayResults();
+        
+    });
+});
+
+// this is the timer countdown function
+function run() {
+
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+}
+
+
+function decrement() {
+
+    //  this will decrease number by one
+    number--;
+
+    //  this will show the number in the #time tag
+    $("#time").html("<h2>Time Remaining: " + number + " Seconds</h2>" + "<br>");
+
+    if (number === 0) {
+
+        // this will stop function to stop timer countdown
+        stop();
+
+        keepingScore();
+        displayResults();
+
+    }
+}
+
+function stop() {
+
+    //  clears intervalId
+    clearInterval(intervalId);
+}
+
+// the function used for displaying results in terms of correct, incorrect, and unanswered. all tags inside div to hide parent div
+function displayResults() {
+
+    $("#time").hide();
+    $("#question1").hide();
+    $("#answer1").hide();
+    $("#question2").hide();
+    $("#answer2").hide();
+    $("#question3").hide();
+    $("#answer3").hide();
+    $("#question4").hide();
+    $("#answer4").hide();
+    $("#question5").hide();
+    $("#answer5").hide();
+    $("#question6").hide();
+    $("#answer6").hide();
+    $("#question7").hide();
+    $("#answer7").hide();
+    $("#submit").hide();
+
+    $("#resultsMessage").html("<h3>All Done!</h3>");
+    $("#correct").html("Correct Answers: " + correctAnswers);
+    $("#incorrect").html("Incorrect Answers: " + incorrectAnswers);
+    $("#unanswered").html("Unanswered: " + unanswered);
+}
+
+// function keeps score in terms of correct, incorrect, and unanswered 
+function keepingScore() {
+
+    var userAnswer1 = $("input[name='answer1']:checked").val();
+    var userAnswer2 = $("input[name='answer2']:checked").val();
+    var userAnswer3 = $("input[name='answer3']:checked").val();
+    var userAnswer4 = $("input[name='answer4']:checked").val();
+    var userAnswer5 = $("input[name='answer5']:checked").val();
+    var userAnswer6 = $("input[name='answer6']:checked").val();
+    var userAnswer7 = $("input[name='answer7']:checked").val();
+
+    // for question 1
+    if (userAnswer1 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer1 == questions[0].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // for question 2
+    if (userAnswer2 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer2 == questions[1].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++; 
+    }
+    // this is tough asf man
+    // for question 3
+    if (userAnswer3 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer3 == questions[2].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // for question 4
+    if (userAnswer4 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer4 == questions[3].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // for question 5
+    if (userAnswer5 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer5 == questions[4].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // for question 6
+    if (userAnswer6 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer6 == questions[5].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // for question 7
+    if (userAnswer7 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer7 == questions[6].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }  
+}
+
+
